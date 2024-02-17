c9a77a771ca6611bbd1eb78ade113ca24ed8b9bf
k1rson <vlad2k15@bk.ru>
2024-02-17 22:35:36 +0300

    2024-02-17 | v1.0.0 | Dev: add dashboards & fix bootstrap markup

--- src/evo/apps/authentication_app/admin.py
+++ src/evo/apps/authentication_app/admin.py
@@ -1,26 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, UserPositionModel
+
+class UserPositionAdmin(admin.ModelAdmin):
+    list_display = ('position_name',)
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     
-    list_display = ('username', 'email', 'is_staff', 'is_active',)
+    list_display = ('username', 'email', 'is_staff', 'is_active', )
     list_filter = ('username', 'email', 'is_staff', 'is_active',)
 
     fieldsets = (
-        ('Общие данные', {'fields': ('username', 'email', 'password', 'src_avatar', 'is_verified_email')}),
+        ('Общие данные', {'fields': ('username', 'email', 'password', 'src_avatar', 'first_name', 'last_name')}),
+        ('Спец. информация', {'fields': ('is_verified_email', 'role', 'position_id')}),
         ('Права администратора', {'fields': ('is_staff', 'is_active')}),
-        ('Группы и права доступа', {'fields': ('role', 'user_permissions')})
     )
 
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': (
-                'username', 'email', 'password1', 'password2', 'is_staff', 'is_verified_email',
-                'is_active', 'role', 'user_permissions'
+                'username', 'email', 'password1', 'password2', 'is_staff', 'is_verified_email'
             )}
         ),
     )
@@ -28,3 +30,4 @@ class CustomUserAdmin(UserAdmin):
     ordering = ('username',)
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(UserPositionModel, UserPositionAdmin)
--- src/evo/apps/authentication_app/models.py
+++ src/evo/apps/authentication_app/models.py
@@ -3,18 +3,28 @@ from django.db import models
 
 from .managers import CustomUserManager
 
+class UserPositionModel(models.Model):
+    position_name = models.CharField('Название должности', max_length=100)
+
+    def __str__(self):
+        return self.position_name
+
+    class Meta:
+        verbose_name_plural = 'Должности'
+
 class CustomUser(AbstractUser):
-    ROLE_CHOICES ={
-        'Accountant': 'Бухгалтер', 
-        'Editor': 'Редактор', 
-        'Admin': 'Администратор'
-    }
+    ROLE_CHOICES = [
+        ('Accountant', 'Бухгалтер'), 
+        ('Editor', 'Редактор'), 
+        ('Admin', 'Администратор')
+    ]
 
     username = models.CharField('Логин', db_index=True, max_length=255, unique=True)
     email = models.EmailField('Email', unique=True)
     is_verified_email = models.BooleanField('Верифицирован ли Email', default=False)
     src_avatar = models.ImageField('Аватар', upload_to='avatars/%Y-%m')
-    role = models.CharField('Роль пользователя', choices=ROLE_CHOICES)
+    role = models.CharField('Роль пользователя', max_length=255, choices=ROLE_CHOICES)
+    position_id = models.ForeignKey(UserPositionModel, verbose_name='Должность пользователя', on_delete=models.CASCADE, null=True, related_name='users')
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ['email']
